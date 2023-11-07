fb872f6886c6e46c898ce5aae37df741ac367d17
bunyanut8115 <bunyanut8115@gmail.com>
2023-11-07 10:52:48 +0700

    Variable.php

---
+++ Variable.php
@@ -0,0 +1,16 @@
+<?php
+    $name = "bonyanut8115";
+    $department = "Business Computer";
+
+    echo $name ." ". $department."<br>";
+
+    $num1 = 100;
+    $num2 = 200;
+    $num3 = 300;
+    $num4 = 400;
+    
+    $sum = $num1 + $num2 + $num3 + $num4;
+    echo "sum = ".$sum. "<br>";
+    $avg = $sum / 4;
+    echo "avg = ".$avg. "<br";
+
