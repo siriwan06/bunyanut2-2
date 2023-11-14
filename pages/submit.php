1056fc3e25e198c0b8f708054effe5196fa7b47d
bunyanut8115 <bunyanut8115@gmail.com>
2023-11-14 11:03:08 +0700

    aA

---
+++ pages/submit.php
@@ -0,0 +1,23 @@
+<?php
+    $email = $_POST['email'];
+    $email = $_POST['password'];
+    $email = $_POST['city'];   
+    $email = $_POST['web'];
+    $email = $_POST['role'];
+    $email = $_POST['sign1'];
+    $email = $_POST['sign2'];
+    $email = $_POST['sign3'];
+
+
+    echo 'email: '.$email.'<br>';
+    echo 'password: '.$password.'<br>';
+    echo 'city: '.$city.'<br>';
+    echo 'web: '.$web.'<br>';
+    echo 'role: '.$role.'<br>';
+    echo 'sign1: '.$sign1.'<br>';
+    echo 'sign2: '.$sign2.'<br>';
+    echo 'sign3: '.$sign3.'<br>';
+
+
+
+?>
